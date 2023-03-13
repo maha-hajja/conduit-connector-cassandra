b216ce8d5cc61c47a4e194820cf232b140ff2bab
maha-hajja <maha.d.hajja@gmail.com>
2023-03-13 13:10:52 +0000

    delete test

--- destination_test.go
+++ destination_test.go
@@ -26,29 +26,3 @@ func TestTeardown_NoOpen(t *testing.T) {
 		t.Errorf("expected no error, got %v", err)
 	}
 }
-
-func Test_Maha(t *testing.T) {
-	// Define the Cassandra cluster configuration
-	clusterConfig := gocql.NewCluster("127.0.0.1")
-	clusterConfig.Keyspace = "store"
-	clusterConfig.ConnectTimeout = time.Second * 5
-	clusterConfig.Consistency = gocql.Quorum
-	clusterConfig.Authenticator = gocql.PasswordAuthenticator{
-		Username: "",
-		Password: "",
-	}
-
-	// Connect to the Cassandra cluster
-	session, err := clusterConfig.CreateSession()
-	if err != nil {
-		panic(err)
-	}
-	defer session.Close()
-
-	// Execute a CQL query
-	var result int
-	if err := session.Query("SELECT item_count FROM shopping_cart WHERE userid = ?", "1234").Scan(&result); err != nil {
-		panic(err)
-	}
-	fmt.Println(result)
-}
